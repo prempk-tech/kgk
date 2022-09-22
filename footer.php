4177a8774a15b32c6f8950392226bd66ad949aeb
prempk-tech <74701984+prempk-tech@users.noreply.github.com>
2022-09-22 12:25:09 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,91 @@
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+
+
+<div class="footer-top" style="background-color: #e6ffff">
+  <div class="container">
+    <div class="row gy-4">
+      <div class="col-lg-5 col-md-12 footer-info">
+        <a href="index.html" class="logo d-flex align-items-center">
+          <img src="assets/img/logo.png" alt="">
+          <span>K.G.KARTHIK</span>
+        </a>
+        <br>
+        <div class="social-links mt-3">
+          <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+          <a target="blank" href="https://instagram.com/sembu_enterprises?igshid=1u8wtxzgkcb7k" class="instagram"><i class="bi bi-instagram bx bxl-instagram"></i></a>
+        </div>
+      </div>
+
+      <div class="col-lg-2 col-6 footer-links">
+        <h4>Useful Links</h4>
+        <ul>
+          <li><i class="bi bi-chevron-right"></i> <a href="dash.php">Dashboard</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="#about ">About us</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="#services">Services</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="#team">Team</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="#register">Register</a></li>
+        </ul>
+      </div>
+
+      <div class="col-lg-2 col-6 footer-links">
+        <h4>Our Services</h4>
+        <ul>
+          <li><i class="bi bi-chevron-right"></i> <a href="#services">Enameld Winding Wire's</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="#services">Submersive Winding Wire's</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="#services">Submersive Spare's</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="#services">Insulating Material's</a></li>
+          <li><i class="bi bi-chevron-right"></i> <a href="#services">more..</a></li>
+        </ul>
+      </div>
+
+      <div align ="end" class="col-lg-3 col-md-12 footer-contact text-center text-md-start">
+        <h4>Contact Us</h4>
+        <p>
+          54C, GK Complex <br>
+          Chittu Kovil,<br>
+          Salem-636001.<br>
+          <strong>Phone:</strong> <br>+91 7708429274, <br> +91 9751357571  <br><br>
+          <strong>Email:</strong> sembuenterprises@gmail.com<br>
+        </p>
+
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="container">
+  <div class="copyright">
+    &copy; Copyright <strong><span>K.G.KARTHIK</span></strong>. All Rights Reserved
+  </div>
+  <div class="credits">
+    <!-- All the links in the footer should remain intact. -->
+    <!-- You can delete the links only if you purchased the pro version. -->
+    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/flexstart-bootstrap-startup-template/ -->
+    Designed by <a href="http://www.dcloudinfotech.com/">Dcloud infotech</a>
+  </div>
+</div>
+</footer><!-- End Footer -->
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+<!-- Vendor JS Files -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.bundle.js"></script>
+<script src="assets/vendor/aos/aos.js"></script>
+<script src="assets/vendor/php-email-form/validate.js"></script>
+<script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+<script src="assets/vendor/purecounter/purecounter.js"></script>
+<script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+<script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+
+
+<!-- Template Main JS File -->
+<script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
